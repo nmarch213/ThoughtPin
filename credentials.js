5950ec147a4fac6db2b37e2a330800f0a61994c0
Nicholas March <n.march213@gmail.com>
2016-11-30 18:50:38 -0600

    init file

--- credentials.js
+++ credentials.js
@@ -2,10 +2,10 @@ module.exports = {
   cookieSecret: 'cookiemonster',
   mongo: {
     development: {
-        connectionString: 'mongodb://youruser:yourpassword@ds027425.mlab.com:27425/project-7'
+        connectionString: 'mongodb://admin:admin@ds115798.mlab.com:15798/blogdbssp'
     },
     production: {
-        connectionString: 'mongodb://youruser:yourpassword@ds027425.mlab.com:27425/project-7'
+        connectionString: 'mongodb://admin:admin@ds115798.mlab.com:15798/blogdbssp'
     }
   }
 }
